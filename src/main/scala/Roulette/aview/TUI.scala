5e89ee4b8461dd8ee4c8e2e6437a3b529a5e4fc5
LeopoldSappler <leopold.sappler@gmail.com>
2022-11-04 08:35:52 +0100

    Tests

--- src/main/scala/Roulette/aview/TUI.scala
+++ src/main/scala/Roulette/aview/TUI.scala
@@ -1,50 +1,16 @@
 package Roulette.aview
 
-import Roulette.controller
-import Roulette.controller.Controller
-import Roulette.model.Player
-import Roulette.model.PlayerBuilder
-import Roulette.util.Observer
-
-import scala.io.StdIn.readLine
-import scala.util.Random
-
-case class TUI(controller: Controller) extends Observer: //player: Player
-
-  controller.add(this)
-
-  inputLoop()
-  val einsatz: Int = 0
-  val r: Int = 0
-  val randZahl: Int = 0
-  def inputLoop(): Unit = { //Interpreter Pattern
-    for (playerIndex <- 0 until controller.getPlayerCount()) {
-      println()
-      print(controller.actualPlayer(playerIndex))
-
-      val einsatz = readLine("Ihr Einsatz: ").toInt
-      val r = new Random()
-      val randZahl = r.nextInt(37)
-
-      val tempPlayer = new PlayerBuilder
-      tempPlayer.withRandZahl(randZahl).withPlayerIndex(playerIndex).withEinsatz(einsatz)
-
-      println()
-
-      println("Willst du eine Zahl (z), gerade oder ungerade (g) oder eine Farbe (f) setzen? \n")
-
-      readLine() match
-        case "z" =>
-          print(controller.num(tempPlayer))
-          println()
-        case "g" =>
-          print(controller.evenOdd(tempPlayer))
-          println()
-        case "f" =>
-          print(controller.colour(tempPlayer))
-          println()
+class TUI {
+    
+    def processInput(input: String): Unit = {
+        input match
+            case "h" =>
+                println("Welcome to Roulette!\nYou can bet by writing 'b'!")
+            case "b" =>
+                println("Bet")
+            case "q" =>
+                println("Quitting...")
+            case _ =>
+                println("Wrong Input")
     }
-    inputLoop()
-  }
-
-  override def update: Unit = println(controller.stateToString())
+}
