97ffb3bbcc8b84b2dff076163ee4ca87c64cd2f1
Nielshen <Niels.hensler@gmail.com>
2022-11-10 13:12:21 +0100

    MVC

--- src/main/scala/Roulette/RTE.scala
+++ src/main/scala/Roulette/RTE.scala
@@ -2,7 +2,7 @@ package Roulette
 
 import Roulette.aview.TUI
 import Roulette.model.Player
-
+import controller.Controller
 import javax.swing.plaf.TextUI
 import scala.io.StdIn.readLine
 import scala.util.Random
@@ -10,8 +10,9 @@ import scala.util.Random
 @main def main(): Unit =
     println("Roulette \n")
     val playercount : Int = readLine("Anzahl Spieler: " ).toInt
-    val tui = TUI(Player(playercount))
-    //val controller = new Controller(Player(playercount))
+    val controller = Controller(playercount).put
+    val tui = TUI(controller)
+    //val tui = TUI(controller)
 //val tui = new Roulette.aview.TUI(controller)
 
 
