8ff6b9b4d10d609a5a79d61a91c7d8259faa0f5f
Nielshen <Niels.hensler@gmail.com>
2022-11-10 12:04:12 +0100

    Interpreter, Fluent Interface Pattern

--- src/main/scala/Roulette/RTE.scala
+++ src/main/scala/Roulette/RTE.scala
@@ -10,8 +10,6 @@ import scala.util.Random
 @main def main(): Unit =
     println("Roulette \n")
     val playercount : Int = readLine("Anzahl Spieler: " ).toInt
-    println("Spieleranzahl: " + playercount)
-    //val player : Player = Player(playercount)
     val tui = TUI(Player(playercount))
     //val controller = new Controller(Player(playercount))
 //val tui = new Roulette.aview.TUI(controller)
--- src/main/scala/Roulette/model/Player.scala
+++ src/main/scala/Roulette/model/Player.scala
@@ -3,7 +3,7 @@ package Roulette.model
 import scala.io.StdIn.readLine
 
 case class Player(val playerCount: Int) {
-  
+
   var players = new Array[Int](playerCount)
   playerStartMoney(players)
   def playerStartMoney(players : Array[Int]): Array[Int] = {
@@ -16,4 +16,24 @@ case class Player(val playerCount: Int) {
     players
   }
   players
+
 }
+
+// Fluent Interface
+class PlayerBuilder:
+  var randZahl: Int = 0
+  var playerIndex: Int = 0
+  var einsatz: Int = 0
+
+  def withRandZahl(randZahl: Int): PlayerBuilder = {
+    this.randZahl = randZahl
+    this
+  }
+  def withPlayerIndex(playerIndex: Int): PlayerBuilder = {
+    this.playerIndex = playerIndex
+    this
+  }
+  def withEinsatz(einsatz: Int): PlayerBuilder = {
+    this.einsatz = einsatz
+    this
+  }
