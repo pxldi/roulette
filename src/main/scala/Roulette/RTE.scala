d45ba375a70b820f791028e6b85c78396265880d
Poldi <leopold.sappler@gmail.com>
2022-11-12 10:54:29 +0100

    Refactored (English language)

--- src/main/scala/Roulette/RTE.scala
+++ src/main/scala/Roulette/RTE.scala
@@ -1,6 +1,7 @@
 package Roulette
 
 import Roulette.aview.TUI
+import Roulette.controller.Controller
 import Roulette.model.Player
 
 import javax.swing.plaf.TextUI
@@ -8,12 +9,8 @@ import scala.io.StdIn.readLine
 import scala.util.Random
 
 @main def main(): Unit =
-    println("Roulette \n")
-    val playercount : Int = readLine("Anzahl Spieler: " ).toInt
-    println("Spieleranzahl: " + playercount)
-    //val player : Player = Player(playercount)
-    val tui = TUI(Player(playercount))
-    //val controller = new Controller(Player(playercount))
-//val tui = new Roulette.aview.TUI(controller)
-
-
+    println("Welcome to Roulette! \n")
+    val playerCount : Int = readLine("How many players are playing: " ).toInt
+    val player = Player(playerCount)
+    val controller = Controller(player)
+    val tui = TUI(controller)
--- src/main/scala/Roulette/model/Player.scala
+++ src/main/scala/Roulette/model/Player.scala
@@ -3,17 +3,36 @@ package Roulette.model
 import scala.io.StdIn.readLine
 
 case class Player(val playerCount: Int) {
-  
   var players = new Array[Int](playerCount)
-  playerStartMoney(players)
-  def playerStartMoney(players : Array[Int]): Array[Int] = {
+  playerStartingMoney(players)
+  def playerStartingMoney(players : Array[Int]): Array[Int] = {
     for (playerIndex <- 0 until playerCount)
-      val money: Int = readLine("Spieler " + playerIndex + " Startkapital: ").toInt
+      val money: Int = readLine("Player " + playerIndex + ": Starting money: $").toInt
       players(playerIndex) = money
       
     for (playerIndex <- 0 until playerCount)
-      println("Spieler " + playerIndex + " Startkapital: " + players(playerIndex))
+      println("Player " + playerIndex + ": Starting money: $" + players(playerIndex))
     players
   }
   players
+
 }
+
+// Fluent Interface
+class PlayerBuilder:
+  var randomNumber: Int = 0
+  var playerIndex: Int = 0
+  var bet: Int = 0
+
+  def withRandZahl(randomNumber: Int): PlayerBuilder = {
+    this.randomNumber = randomNumber
+    this
+  }
+  def withPlayerIndex(playerIndex: Int): PlayerBuilder = {
+    this.playerIndex = playerIndex
+    this
+  }
+  def withEinsatz(bet: Int): PlayerBuilder = {
+    this.bet = bet
+    this
+  }
