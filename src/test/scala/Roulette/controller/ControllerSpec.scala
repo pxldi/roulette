4dc5b52f2f70c4c02f7d1d0f105d20d30b065530
Leopold Sappler <leopold.sappler@gmail.com>
2022-11-18 08:28:48 +0100

    AnyWordSpec updated

--- src/test/scala/Roulette/controller/ControllerSpec.scala
+++ src/test/scala/Roulette/controller/ControllerSpec.scala
@@ -1,9 +1,11 @@
 package Roulette.controller
 
 import main.scala.Roulette.controller.Controller
-import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.matchers.should
+import org.scalatest.wordspec.AnyWordSpec
 
-class controllerSpec extends WordSpec with Matchers {
+
+class controllerSpec extends AnyWordSpec with should.Matchers {
   "A controller" should{
     val player = Player(2)
     val controller = Controller(player)
