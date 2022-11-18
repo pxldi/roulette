194cb14656ab9f004dfdfb968e31811183f90163
Leopold Sappler <leopold.sappler@gmail.com>
2022-11-18 08:38:58 +0100

    controllerspec

--- src/test/scala/Roulette/controller/ControllerSpec.scala
+++ src/test/scala/Roulette/controller/ControllerSpec.scala
@@ -7,9 +7,9 @@ import org.scalatest.wordspec.AnyWordSpec
 
 
 class controllerSpec extends AnyWordSpec with should.Matchers {
-  "A controller" should{
     val player = Player(2)
     val controller = Controller(player)
+  "A controller" should{
     "have return value in stateToString" in {
       controller.stateToString() should be("Test Update")
     }
