10bf7322333a2d9de3c06dec9d355b725da36925
Leopold Sappler <leopold.sappler@gmail.com>
2022-11-18 08:53:15 +0100

    tests

--- src/test/scala/Roulette/model/PlayerSpec.scala
+++ src/test/scala/Roulette/model/PlayerSpec.scala
@@ -1,1 +1,16 @@
+package Roulette.model
 
+import Roulette.model.Player
+import org.scalatest.matchers.should
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalactic.TypeCheckedTripleEquals
+
+class PlayerSpec extends AnyWordSpec with should.Matchers with TypeCheckedTripleEquals {
+  "A player" should{
+    "have player array" in {
+      var actual = new Array[Int](playerCount)
+      var expected = new Array[Int](playerCount)
+      actual should ===(expected)
+    }
+  }
+}
