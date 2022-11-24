b1e09ba5b296ce12bc611ab17e6dcf84841b4f7e
Nielshen <Niels.hensler@gmail.com>
2022-11-24 17:00:37 +0100

    Player Tests

--- src/test/scala/Roulette/model/PlayerSpec.scala
+++ src/test/scala/Roulette/model/PlayerSpec.scala
@@ -1,6 +1,8 @@
-package Roulette.model
+package Roulette.controller
 
 import Roulette.controller.State.{IDLE, State}
+import Roulette.controller
+
 import Roulette.model.Bet
 import Roulette.model.Player
 
@@ -18,9 +20,25 @@ import scala.Console.in
 
 class PlayerSpec extends AnyWordSpec with should.Matchers with TypeCheckedTripleEquals {
 
+  val playerCount : Int = 1
+  val startingMoney : Int = 100
+  val controller = Controller(playerCount, startingMoney)
+  val vc = VectorBuilder[Bet]
+  val bets = controller.calculateBets(vc.result())
+
+  var players = Vector[Player]()
+  controller.setupPlayers()
+
   "A Player" should {
     "have a method to get his available Money" in {
-      
+      /*val money = players(0).getAvailableMoney()
+      val expected = 100
+      money should ===(expected)*/
+    }
+  }
+  "A Bet" should {
+    "have a method to get Bettype" in {
+
     }
   }
 }
