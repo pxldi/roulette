325c6752b6c900eb565467d37462f2d8f58bc10c
LeopoldSappler <leopold.sappler@gmail.com>
2022-11-25 09:00:58 +0100

    Tests

--- src/test/scala/Roulette/controllerSpec/ControllerSpec.scala
+++ src/test/scala/Roulette/controllerSpec/ControllerSpec.scala
@@ -30,7 +30,8 @@ class ControllerSpec extends AnyWordSpec with should.Matchers with TypeCheckedTr
       }
       val expected = vc.result()
       val result = controller.players
-      result should ===(expected)
+      result.size should be(expected.size)
+      result(0).getAvailableMoney() should be(expected(0).getAvailableMoney())
     }
   }
 
@@ -79,14 +80,27 @@ class ControllerSpec extends AnyWordSpec with should.Matchers with TypeCheckedTr
       controller.printState() should ===(expected)
     }
 
-    /*"have a method to get Num"in {
-      for (bet <- bets) {
-        val num = controller.num(bet)
-        val expected = 1
-        num should ===(expected)
-      }
-    }*/
-    //TODO Color, EvenOdd, Num, Interpreter
+    "have a method to decide between the colours" in {
+      val bet = new Bet
+      bet.withBetType("c").withRandomNumber(12).withPlayerIndex(0).withBetAmount(20).withColor("r")
+      val colour = controller.color(bet)
+      val expected = "Player 1 won their bet of $40. They now have $520 available."
+      colour should ===(expected)
+    }
+    "have a method to decide between the number" in {
+      val bet = new Bet
+      bet.withBetType("n").withRandomNumber(12).withPlayerIndex(0).withBetAmount(20).withBetNumber(12)
+      val num = controller.num(bet)
+      val expected = "Player 1 won their bet of $720. They now have $1760 available."
+      num should ===(expected)
+    }
+    "have a method to decide between even and odd" in {
+      val bet = new Bet
+      bet.withBetType("o").withRandomNumber(12).withPlayerIndex(0).withBetAmount(20).withBetNumber(12)
+      val evenOdd = controller.num(bet)
+      val expected = "Player 1 won their bet of $720. They now have $4240 available."
+      evenOdd should ===(expected)
+    }
 
   }
 }
---
+++ src/test/scala/Roulette/model/BetSpec.scala
@@ -0,0 +1,30 @@
+package Roulette.model
+
+import Roulette.controller.State.{IDLE, State}
+import Roulette.model.{Player, Bet}
+
+import scala.io.StdIn.readLine
+import scala.collection.immutable.VectorBuilder
+import org.scalactic.Equality
+import org.scalactic.TolerantNumerics
+import org.scalactic.TypeCheckedTripleEquals
+import org.scalatest.matchers.should
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.EitherValues
+import org.scalactic.TripleEquals.unconstrainedEquality
+
+class BetSpec extends AnyWordSpec with should.Matchers with TypeCheckedTripleEquals {
+    "A Bet" should {
+        "be constructed empty" in {
+            val bet = new Bet
+            bet.withBetType("").withRandomNumber(0).withPlayerIndex(0).withBetNumber(0).withOddOrEven("").withColor("").withBetAmount(0)
+            bet.bet_type should be("")
+            bet.random_number should be(0)
+            bet.player_index should be(0)
+            bet.bet_number should be(0)
+            bet.bet_odd_or_even should be("")
+            bet.bet_color should be("")
+            bet.bet_amount should be(0)
+        }
+    }
+}
