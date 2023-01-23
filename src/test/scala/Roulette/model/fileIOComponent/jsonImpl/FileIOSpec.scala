98dfce03a2a7b21cbf80b82f84fe7677b63f848e
Nielshen <Niels.hensler@gmail.com>
2023-01-23 19:43:24 +0100

    FileIo Tests

---
+++ src/test/scala/Roulette/model/fileIOComponent/jsonImpl/FileIOSpec.scala
@@ -0,0 +1,30 @@
+package Roulette.model.fileIOComponent.jsonImpl
+
+import Roulette.controller.controllerComponent.ControllerInterface
+import Roulette.controller.controllerComponent.controllerBaseImpl.Controller
+import Roulette.model.Bet
+import Roulette.model.fileIOComponent.FileIOInterface
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers.*
+import scala.collection.immutable.VectorBuilder
+
+class FileIOSpec extends AnyWordSpec {
+  "Json IO" should {
+    val playerCount: Int = 1
+    val startingMoney: Int = 100
+    val fIO = new FileIO
+    given FileIOInterface = fIO
+    val controller = new Controller
+    given ControllerInterface = controller
+    controller.generateRandomNumber()
+    controller.setupPlayers()
+    val vc = VectorBuilder[Bet]
+    val bets = controller.calculateBets()
+    controller.setupPlayers()
+    "after saving load" in {
+      controller.save()
+      controller.load()
+    }
+  }
+}
+
